d260f434ed379fbccf29ef43689d9001141e84ad
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-03-13 13:35:19 +0700

    fix: correct typo in CSS layer name and enhance Progress component header styling

--- src/feature/auth/form/components/Progress.tsx
+++ src/feature/auth/form/components/Progress.tsx
@@ -3,7 +3,7 @@ import { Card, CardHeader } from "@/shared/component/ui/card";
 const Progress = () => {
   return (
     <Card className="px-12">
-      <CardHeader>Langkah 1 dari 3</CardHeader>
+      <CardHeader className="text-xl-bold">Langkah 1 dari 3</CardHeader>
     </Card>
   );
 };

594a0cbfadd90326bacecae8d8aa186401f131fc
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-03-29 22:32:01 +0700

    feat: enhance registration success page with improved layout and dynamic progress indicator

--- src/feature/auth/form/components/form-2/RegisterSuccessSection.tsx
+++ src/feature/auth/form/components/form-2/RegisterSuccessSection.tsx
@@ -5,31 +5,35 @@ import Link from "next/link";
 
 const RegisterSuccessSection = () => {
   return (
-    <section className="flex h-screen items-center justify-center bg-green-50">
-      <div className="mx-auto h-auto w-full max-w-[90%] space-y-4 rounded-[40px] border-3 border-green-900 bg-white p-4 py-6 shadow-[2px_2px_0px_0px_var(--color-green-900)] md:max-w-[35%] md:p-10 md:py-12 md:shadow-[4px_4px_0px_0px_var(--color-green-900)]">
-        <div className="flex flex-col items-center justify-center gap-12">
-          <video autoPlay muted playsInline className="h-20 w-20">
+    <section className="flex items-center justify-center bg-green-50 py-20">
+      <div className="mx-auto h-auto w-full max-w-[90%] rounded-[40px] border-3 border-green-900 bg-white p-4 py-6 shadow-[2px_2px_0px_0px_var(--color-green-900)] md:max-w-[35%] md:p-10 md:pb-12 md:shadow-[4px_4px_0px_0px_var(--color-green-900)]">
+        <div className="mx-auto w-[70%] md:w-[50%]">
+          <video autoPlay muted playsInline className="mx-auto">
             <source src="/animation/check.webm" type="video/webm" />
           </video>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-6 md:gap-12">
           <div className="flex flex-col items-center gap-2.5">
-            <p className="text-2xl-bold text-green-900">
+            <p className="text-[12px] font-bold text-green-900 md:text-2xl">
               Pendaftaran berhasil!
             </p>
-            <p className="text-sm-medium mx-auto w-[90%] text-center text-green-500">
+            <p className="mx-auto w-[90%] text-center text-[8px] font-medium text-green-500 md:text-sm">
               Tim kami akan meninjau pengajuan profil mitra Anda dalam 2-3 hari
               kerja. Kami akan memberikan kabar melalui email Anda segera.
             </p>
           </div>
           <SuccessProgress />
           <Link href="/" className="block w-full">
-            <Button className="group relative w-full cursor-pointer overflow-hidden border border-green-900 bg-orange-100 py-6 text-xl font-bold text-green-800 transition-all duration-300">
+            <Button className="group relative w-full cursor-pointer overflow-hidden rounded-[8px] border border-green-900 bg-orange-100 py-4 text-[12px] font-bold text-green-800 transition-all duration-300 md:py-6 md:text-xl">
               <span className="absolute inset-0 z-0 origin-left scale-x-0 bg-orange-600 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
 
-              <p className="relative z-10 transition-colors duration-300 group-hover:text-white">
-                Kembali ke Beranda
-              </p>
+              <div className="flex items-center gap-2 md:gap-8">
+                <p className="relative z-10 transition-colors duration-300 group-hover:text-white">
+                  Kembali ke Beranda
+                </p>
 
-              <House className="absolute right-12 z-10 h-8 w-8 transition-all duration-300 group-hover:scale-110 group-hover:text-white" />
+                <House className="z-10 transition-all duration-300 group-hover:scale-110 group-hover:text-white" />
+              </div>
             </Button>
           </Link>
         </div>
--- src/feature/auth/form/components/form-2/SuccessProgress.tsx
+++ src/feature/auth/form/components/form-2/SuccessProgress.tsx
@@ -1,30 +1,58 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/shared/component/ui/card";
 
 const SuccessProgress = () => {
+  const [activeStep, setActiveStep] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveStep((prev) => {
+        if (prev >= 3) return prev;
+        return prev + 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const getCircleStyle = (step: number) => {
+    return step <= activeStep
+      ? "bg-orange-600 text-orange-50"
+      : "bg-orange-200 text-orange-50";
+  };
+
   return (
     <Card className="w-full border border-green-900 bg-orange-50">
-      <CardContent className="flex flex-col gap-2">
+      <CardContent className="flex flex-col gap-1 md:gap-2">
         <div className="flex items-center gap-4">
-          <p className="rounded-full bg-orange-600 p-2 px-3.5 text-[12px] font-bold text-orange-50">
+          <p className="rounded-full bg-orange-600 p-1.5 px-2.5 text-[8px] font-bold text-orange-50 md:p-2 md:px-3.5 md:text-[12px]">
             1
           </p>
-          <p className="text-sm font-bold text-green-900">Pengajuan Berhasil</p>
+          <p className="text-[8px] font-bold text-green-900 md:text-sm">
+            Pengajuan Berhasil
+          </p>
         </div>
-        <div className="ml-4 h-4 w-0.5 bg-green-900" />
+        <div className="ml-2.5 h-4 w-0.5 bg-green-900 md:ml-4 md:h-4" />
         <div className="flex items-center gap-4">
-          <p className="rounded-full bg-orange-200 p-2 px-3.5 text-[12px] font-bold text-orange-50">
+          <p
+            className={`rounded-full p-1.5 px-2.5 text-[8px] font-bold transition-all duration-500 md:p-2 md:px-3.5 md:text-[12px] ${getCircleStyle(2)}`}
+          >
             2
           </p>
-          <p className="text-sm font-bold text-green-900">
+          <p className="text-[8px] font-bold text-green-900 md:text-sm">
             Proses Peninjauan Internal
           </p>
         </div>
-        <div className="ml-4 h-4 w-0.5 bg-green-900" />
+        <div className="ml-2.5 h-4 w-0.5 bg-green-900 md:ml-4 md:h-4" />
         <div className="flex items-center gap-4">
-          <p className="rounded-full bg-orange-200 p-2 px-3.5 text-[12px] font-bold text-orange-50">
+          <p
+            className={`rounded-full p-1.5 px-2.5 text-[8px] font-bold transition-all duration-500 md:p-2 md:px-3.5 md:text-[12px] ${getCircleStyle(3)}`}
+          >
             3
           </p>
-          <p className="text-sm font-bold text-green-900">
+          <p className="text-[8px] font-bold text-green-900 md:text-sm">
             Aktivasi Akun Kemitraan
           </p>
         </div>
